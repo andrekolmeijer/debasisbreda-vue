8c515d8487e4bd1d54d822d74b585442e8b07e9f
Andre Kolmeijer <19167112+andrekolmeijer@users.noreply.github.com>
2023-06-17 18:46:02 +0200

    style: crop img

--- src/components/WelcomeBanner.vue
+++ src/components/WelcomeBanner.vue
@@ -9,12 +9,12 @@ const smAndLarger = breakpoints.greater('sm')
   <div class="overflow-hidden sm:rounded">
     <img
       v-if="smAndLarger"
-      class="h-[302px] w-full object-cover object-bottom"
+      class="h-[225px] w-full object-cover object-top"
       src="../assets/img/banner.jpg"
       alt="Vooraanzicht van Inspire Breda" />
     <img
       v-else
-      class="h-80 w-full object-cover object-bottom"
+      class="h-48 w-full object-cover object-top"
       src="../assets/img/banner-mobile.jpg"
       alt="Vooraanzicht van Inspire Breda" />
   </div>
