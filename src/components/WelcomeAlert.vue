547702e8068550324808fd1684eb01f3c034673c
Andre Kolmeijer <19167112+andrekolmeijer@users.noreply.github.com>
2023-06-17 19:57:12 +0200

    fix: remove a tag

--- src/components/WelcomeAlert.vue
+++ src/components/WelcomeAlert.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="bg-red-500">
     <div class="max-w-5xl m-auto group">
-      <a href="#openingstijden" rel="nofollow">
-        <p class="px-4 py-4 text-center text-inspire-white text-sm font-light">
-          <span class="font-semibold">Let op:</span> Inspire Coffee Company is nu <span class="font-semibold">De Basis!</span> Kom naar de <span class="font-semibold">officiële opening</span> op <span class="font-semibold">Za. 17 juni</span> om <span class="font-semibold">16:00 uur!</span><br> <span class="italic">Een volledig nieuwe website is onder constructie.</span>
-        </p>
-      </a>
+      <p class="px-4 py-4 text-center text-inspire-white text-sm font-light">
+        <span class="font-semibold">Let op:</span> Inspire Coffee Company is nu <span class="font-semibold">De Basis!</span> Kom naar de <span class="font-semibold">officiële opening</span> op <span class="font-semibold">Za. 17 juni</span> om <span class="font-semibold">16:00 uur!</span><br> <span class="italic">Een volledig nieuwe website is onder constructie.</span>
+      </p>
     </div>
   </div>
 </template>
