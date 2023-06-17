231881c9750234c71d7d8205399f59437381e4ff
Andre Kolmeijer <19167112+andrekolmeijer@users.noreply.github.com>
2023-06-17 18:52:13 +0200

    ref: change alert msg

--- src/components/WelcomeAlert.vue
+++ src/components/WelcomeAlert.vue
@@ -3,7 +3,7 @@
     <div class="max-w-5xl m-auto group">
       <a href="#openingstijden" rel="nofollow">
         <p class="px-4 py-4 text-center text-inspire-white text-sm font-light">
-          <span class="font-semibold">Let op:</span> Inspire Coffee Company is nu <span class="font-semibold">De Basis!</span> De nieuwe website is onder constructie en komt eraan. <br>Kom naar de <span class="font-semibold">officiële opening</span> op <span class="font-semibold">Za. 17 juni</span> om <span class="font-semibold">16:00 uur!</span>
+          <span class="font-semibold">Let op:</span> Inspire Coffee Company is nu <span class="font-semibold">De Basis!</span> Kom naar de <span class="font-semibold">officiële opening</span> op <span class="font-semibold">Za. 17 juni</span> om <span class="font-semibold">16:00 uur!</span><br> <span class="italic">Een volledig nieuwe website is onder constructie.</span>
         </p>
       </a>
     </div>
