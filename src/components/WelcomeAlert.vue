887f5bced5cab47c8b0a03af6966ac9587674e0d
Andre Kolmeijer <19167112+andrekolmeijer@users.noreply.github.com>
2023-06-17 19:59:42 +0200

    style: change color

--- src/components/WelcomeAlert.vue
+++ src/components/WelcomeAlert.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-red-500">
+  <div class="bg-inspire-orange-dark">
     <div class="max-w-5xl m-auto group">
       <p class="px-4 py-4 text-center text-inspire-white text-sm font-light">
         <span class="font-semibold">Let op:</span> Inspire Coffee Company is nu <span class="font-semibold">De Basis!</span> Kom naar de <span class="font-semibold">officiële opening</span> op <span class="font-semibold">Za. 17 juni</span> om <span class="font-semibold">16:00 uur!</span><br> <span class="italic">Een volledig nieuwe website is onder constructie.</span>
