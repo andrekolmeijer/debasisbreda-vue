77da99407ded4b00b3568ea4d78ed60b1e4308d8
Andre Kolmeijer <19167112+andrekolmeijer@users.noreply.github.com>
2023-06-17 20:35:44 +0200

    ref: change alert msg

--- src/components/WelcomeAlert.vue
+++ src/components/WelcomeAlert.vue
@@ -2,7 +2,7 @@
   <div class="bg-inspire-orange-dark">
     <div class="max-w-5xl m-auto group">
       <p class="px-4 py-4 text-center text-inspire-white text-sm font-light">
-        <span class="font-semibold">Let op:</span> Inspire Coffee Company is nu <span class="font-semibold">De Basis!</span> Kom naar de <span class="font-semibold">officiële opening</span> op <span class="font-semibold">Za. 17 juni</span> om <span class="font-semibold">16:00 uur!</span><br> <span class="italic">Een volledig nieuwe website is onder constructie.</span>
+        <span class="font-semibold">Inspire Coffee Company</span> is nu <span class="font-semibold">De Basis!</span> Kom naar de <span class="font-semibold">officiële opening</span> op <span class="font-semibold">Za. 17 juni</span> om <span class="font-semibold">16:00 uur!</span><br> <span class="italic">Een volledig nieuwe website is onder constructie.</span>
       </p>
     </div>
   </div>
