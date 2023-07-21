6bb6b4723803740b82b4a43799e6c52575a6dd8a
Andre Kolmeijer <19167112+andrekolmeijer@users.noreply.github.com>
2023-07-21 21:27:33 +0200

    style: change bg color

--- src/components/WelcomeAlert.vue
+++ src/components/WelcomeAlert.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-inspire-orange-dark">
+  <div class="bg-inspire-brown-light">
     <div class="max-w-5xl m-auto group">
       <p class="px-4 md:px-8 py-4 text-center text-inspire-white text-sm font-light">
         <span class="font-semibold">Inspire Coffee Company</span> is nu <span class="font-semibold">De Basis!</span> Nieuwe huistijl. Zelfde biertjes, cocktails en nog steeds geweldige koffie!
