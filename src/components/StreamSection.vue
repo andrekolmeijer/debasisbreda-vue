bdc62747551a2f8c43c6e8b25aebe34e124b6dbc
Andre Kolmeijer <19167112+andrekolmeijer@users.noreply.github.com>
2023-07-21 20:30:52 +0200

    style: change section text + title

--- src/components/StreamSection.vue
+++ src/components/StreamSection.vue
@@ -4,10 +4,10 @@
   <section class="text-inspire-white bg-inspire-brown-light">
     <div class="m-auto px-4 md:px-8 py-16 max-w-5xl">
       <h2 class="text-2xl font-black uppercase tracking-normal text-center">
-        Live streams!
+        Livestream!
       </h2>
       <p class="pt-8 text-base font-light text-center">
-        Word er bij ons gedraaid? Dan kun je dat overal <span class="font-semibold underline underline-offset-2">live</span> volgen via <a class="" href="https://www.twitch.tv/basisbreda"><img class=" inline-block h-6" src="../assets/img/TwitchExtrudedWordmarkPurple.png" alt="Twitch logo"></a> of op onze website.
+        Word er bij ons gedraaid? Dan kun je dat <span class="font-semibold underline underline-offset-2">live</span> volgen via <a class="" href="https://www.twitch.tv/basisbreda"><img class=" inline-block h-6" src="../assets/img/TwitchExtrudedWordmarkPurple.png" alt="Twitch logo"></a> of op onze website.
       </p>
       <div class="pt-8 flex items-center justify-center">
         <iframe
