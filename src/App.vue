7423c37890f82783a148b472683d7966b3c7bcd1
Andre Kolmeijer <19167112+andrekolmeijer@users.noreply.github.com>
2023-07-24 13:05:01 +0200

    ref: change mobile nav

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ const { socials } = useDataStore()
 <template>
   <WelcomeAlert />
   <header class="m-auto flex max-w-5xl items-center justify-between px-4 py-8 md:px-8">
-    <div class="h-10">
+    <div class="h-10 pr-8 shrink grow-0">
       <img
         class="h-full object-cover hidden sm:block"
         src="./assets/img/Logo_DeBasis_bDeBasis_Cyan_Cyan_Dark.webp"
@@ -26,51 +26,57 @@ const { socials } = useDataStore()
         src="./assets/img/Logo_DeBasis_b_Cyan.webp"
         alt="Logo De Basis" />
     </div>
-    <nav class="pt-[12px]">
-      <ul class="flex items-center space-x-4 sm:space-x-8">
-        <li class="hidden sm:block">
-          <a
-            class="py-1 transition-colors duration-200 font-semibold text-inspire-orange-dark hover:text-inspire-brown-light focus:text-inspire-brown-light active:text-inspire-orange-dark"
-            href="#menu"
-            rel="nofollow">
-            Menu
-          </a>
-        </li>
-        <li class="hidden sm:block md:pr-8">
-          <a
-            class="py-1 transition-colors duration-200 font-semibold text-inspire-orange-dark hover:text-inspire-brown-light focus:text-inspire-brown-light active:text-inspire-orange-dark"
-            href="#contact"
-            rel="nofollow">
-            Contact
-          </a>
-        </li>
-        <li class="block sm:hidden">
-          <a
-            class="py-4 transition-colors duration-200 text-inspire-orange-dark hover:text-inspire-brown-light focus:text-inspire-brown-light active:text-inspire-orange-dark"
-            href="#menu">
-            <IconMenu class="!h-6 !w-6" />
-          </a>
-        </li>
-        <li class="block sm:hidden">
-          <a
-            class="py-4 transition-colors duration-200 text-inspire-orange-dark hover:text-inspire-brown-light focus:text-inspire-brown-light active:text-inspire-orange-dark"
-            href="#contact">
-            <IconContact class="!h-6 !w-6" />
-          </a>
-        </li>
-        <li
-          v-for="(social, index) in socials"
-          :key="index"
-          class="ml-4 transition-opacity duration-200 text-inspire-brown-light first:ml-0 hover:text-inspire-orange focus:text-inspire-orange active:text-inspire-brown-light">
-          <a
-            :href="social.href"
-            target="_blank"
-            :aria-label="social.label">
-            <IconFacebook v-if="social.label == 'facebook'" class="!h-6 !w-6" />
-            <IconInstagram v-if="social.label == 'instagram'" class="!h-6 !w-6" />
-            <IconTwitch v-if="social.label == 'twitch'" class="!h-6 !w-6" />
-          </a>
-        </li>
+    <nav class="pt-[12px] shrink-0 grow sm:shrink sm:grow-0">
+      <ul class="flex items-center justify-between">
+        <div class="hidden sm:flex items-center space-x-4 sm:space-x-8">
+          <li>
+            <a
+              class="py-1 transition-colors duration-200 font-semibold text-inspire-orange-dark hover:text-inspire-brown-light focus:text-inspire-brown-light active:text-inspire-orange-dark"
+              href="#menu"
+              rel="nofollow">
+              Menu
+            </a>
+          </li>
+          <li class="sm:pr-8">
+            <a
+              class="py-1 transition-colors duration-200 font-semibold text-inspire-orange-dark hover:text-inspire-brown-light focus:text-inspire-brown-light active:text-inspire-orange-dark"
+              href="#contact"
+              rel="nofollow">
+              Contact
+            </a>
+          </li>
+        </div>
+        <div class="flex sm:hidden items-center space-x-4 sm:space-x-8">
+          <li>
+            <a
+              class="py-4 transition-colors duration-200 text-inspire-orange-dark hover:text-inspire-brown-light focus:text-inspire-brown-light active:text-inspire-orange-dark"
+              href="#menu">
+              <IconMenu class="!h-6 !w-6" />
+            </a>
+          </li>
+          <li class="pr-4">
+            <a
+              class="py-4 transition-colors duration-200 text-inspire-orange-dark hover:text-inspire-brown-light focus:text-inspire-brown-light active:text-inspire-orange-dark"
+              href="#contact">
+              <IconContact class="!h-6 !w-6" />
+            </a>
+          </li>
+        </div>
+        <div class="flex items-center space-x-4 sm:space-x-8">
+          <li
+            v-for="(social, index) in socials"
+            :key="index"
+            class="ml-4 transition-opacity duration-200 text-inspire-brown-light first:ml-0 hover:text-inspire-orange focus:text-inspire-orange active:text-inspire-brown-light">
+            <a
+              :href="social.href"
+              target="_blank"
+              :aria-label="social.label">
+              <IconFacebook v-if="social.label == 'facebook'" class="!h-6 !w-6" />
+              <IconInstagram v-if="social.label == 'instagram'" class="!h-6 !w-6" />
+              <IconTwitch v-if="social.label == 'twitch'" class="!h-6 !w-6" />
+            </a>
+          </li>
+        </div>
       </ul>
     </nav>
   </header>
